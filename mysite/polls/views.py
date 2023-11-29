1b78b1d2cf36998839afc60ed06d3efa55cae9b4
Yerin Kim <yrkim.me@gmail.com>
2023-11-29 20:31:28 +0900

    Polls 애플리케이션을 클래스형 뷰 / 템플릿 상속으로 변경

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,18 +3,30 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from polls.models import Question, Choice
 from polls.forms import NameForm
-# Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
+# def index(request):
+#     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 
@@ -34,9 +46,12 @@ def vote(request, question_id):
 
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 
