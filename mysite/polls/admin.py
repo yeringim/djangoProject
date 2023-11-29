29c8eaccbd874e9746bb8456ca1f37266179d796
Yerin Kim <yrkim.me@gmail.com>
2023-11-29 21:14:46 +0900

    Django Admin 사이트 수정

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from polls.models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
+from polls.models import Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Statement', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline] # Choice 모델 클래스 같이 보기
+    list_display = ('question_text', 'pub_date') # 레코드 리스트 컬럼 항목 지정
+    list_filter = ['pub_date'] # 필터 사이드바 추가
+    search_fields = ['question_text'] # 검색 박스 추가
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
